05a3be748ef212d760e4f5929616408486579c4c
YonnaR <yoann.forthulman@gmail.com>
2019-09-16 00:55:08 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import UseMailInput from './Hooks/UseMailInput';
 
 const App: React.FC = () => {
+  
+  const [mail, onMailChange, isErrorOnMail, IsMailValid] = UseMailInput()
+  const [tel, onTelChange, isErrorOnTel, IsTelValid] = UseMailInput()
+  const [monney, onMonneyChange, isErrorOnMonney, IsMonneyValid] = UseMailInput()
+
+  useEffect(()=>{
+    console.log({
+      "mail":{
+        mail, isErrorOnMail, IsMailValid
+      },
+      "tel":{
+        tel, isErrorOnTel, IsTelValid
+      },
+      "money":{
+        monney, isErrorOnMonney, IsMonneyValid 
+      }
+    })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{
+      display:"flex",
+      justifyContent:"center",
+      flexDirection:"column"
+    }}>
+
+      <div style={DivStyles}>
+        <label>This is a mail input</label>
+        <input type="text" value={mail} onChange={(e)=>onMailChange(e.target.value)}/>
+      </div>
+
+      <div style={DivStyles}>
+        <label> this is a tel input </label>
+        <input type="text" value={tel} onChange={(e)=>onTelChange(e.target.value)}/>
+      </div>
+
+      <div style={DivStyles}>
+        <label> this is a money input </label>
+        <input type="text" value={monney} onChange={(e)=>onMonneyChange(e.target.value)}/>
+      </div>
+
+
     </div>
   );
+}
+
+const DivStyles = {
+  margin: "2rem"
 }
 
 export default App;
---
+++ src/Hooks/UseTelInput.tsx
@@ -0,0 +1,33 @@
+import React,{ useEffect, useState } from "react";
+
+const UseTelInput = () =>{
+
+  const [ value , setValue ] = useState<string>('')
+  const [ isError, setIsError ] = useState<boolean>(false)
+  const [ isValid, setIsValid ] = useState<boolean>(false)
+
+  useEffect(()=>{
+    let isItValid = isNewValueIsCorrect(value)
+    //traitement goes here
+    if( !isItValid ){
+      setIsError(true)
+      setIsValid(false)
+    } 
+    else{
+      setIsValid(true)
+      setIsError(false)
+    }
+  },[value])
+
+  const isNewValueIsCorrect :(s :string)=>boolean = s =>{
+
+    return true
+  }
+  
+  const onChange :( e: string )=>void = e => {if(e) setValue(e)}
+
+  
+  return [ value, onChange, isError, isValid ]
+}
+
+export default UseTelInput
